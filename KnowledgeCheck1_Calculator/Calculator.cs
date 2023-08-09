3aa7aa88e7ed8fbc8d9c5a56f453aca995bf9e0b
Flarkinater <flarkinater@gmail.com>
2023-08-08 21:26:38 -0400

    Added Multiply functionality. Added GetNumber() method to handle user input for numbers

--- KnowledgeCheck1_Calculator/Calculator.cs
+++ KnowledgeCheck1_Calculator/Calculator.cs
@@ -8,19 +8,19 @@ namespace KnowledgeCheck1_Calculator
 {
     public class Calculator
     {
-        public int Add(int first, int second)
+        public double Add(double first, double second)
         {
             return first + second;
         }
 
-        public int Subtract(int first, int second)
+        public double Subtract(double first, double second)
         {
             return first - second;
         }
 
-        public int Multiply(int first, int second)
+        public double Multiply(double first, double second)
         {
-            throw new NotImplementedException();
+            return first * second;
         }
 
         public double Divide(double first, double second)
--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KnowledgeCheck1_Calculator
 {
@@ -10,71 +6,73 @@ namespace KnowledgeCheck1_Calculator
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello. Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division");
-
-            var input = Console.ReadLine();
+            var input = "";
             var calculator = new Calculator();
+            double number1;
+            double number2;
 
-            switch (input)
+            while (true)
             {
-                case "1":
-                    Console.WriteLine("Enter 2 integers to add");
-                    var addNumber1 = Console.ReadLine();
-                    var addNumber2 = Console.ReadLine();
+                Console.WriteLine("Type 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division. Type \"exit\" to close the calculator.");
+                input = Console.ReadLine();
+                if (input == "exit") break;
 
-                    if (int.TryParse(addNumber1, out int addNumOne) && int.TryParse(addNumber2, out int addNumTwo))
-                    {
-                        Console.Write($"{addNumber1} + {addNumber2} = ");
-                        Console.Write(calculator.Add(addNumOne, addNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
-                    }
-                    break;
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Enter 2 numbers to add");
 
-                case "2":
-                    Console.WriteLine("Enter 2 integers to subtract");
-                    var subtractNumber1 = Console.ReadLine();
-                    var subtractNumber2 = Console.ReadLine();
+                        number1 = GetNumber();
+                        number2 = GetNumber();
 
-                    if (int.TryParse(subtractNumber1, out int subNumOne) && int.TryParse(subtractNumber2, out int subNumTwo))
-                    {
-                        Console.Write($"{subtractNumber1} - {subtractNumber2} = ");
-                        Console.Write(calculator.Subtract(subNumOne, subNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
-                    }
-                    break;
+                        Console.WriteLine($"{number1} + {number2} = {calculator.Add(number1, number2)}");
+                        break;
 
-                case "3":
-                    // Add code here
-                    break;
+                    case "2":
+                        Console.WriteLine("Enter 2 numbers to subtract");
 
-                case "4":
-                    Console.WriteLine("Enter 2 integers to divide");
-                    var divideNumber1 = Console.ReadLine();
-                    var divideNumber2 = Console.ReadLine();
+                        number1 = GetNumber();
+                        number2 = GetNumber();
 
-                    if (double.TryParse(divideNumber1, out double divNumOne) && double.TryParse(divideNumber2, out double divNumTwo))
-                    {
-                        Console.Write($"{divideNumber1} / {divideNumber2} = ");
-                        Console.Write(calculator.Divide(divNumOne, divNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
-                    }
-                    break;
-                    break;
+                        Console.WriteLine($"{number1} - {number2} = {calculator.Subtract(number1, number2)}");
+                        break;
 
-                default:
-                    Console.WriteLine("Unknown input");
-                    break;
+                    case "3":
+                        Console.WriteLine("Enter 2 numbers to multiply");
+
+                        number1 = GetNumber();
+                        number2 = GetNumber();
+
+                        Console.WriteLine($"{number1} * {number2} = {calculator.Multiply(number1, number2)}");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Enter 2 numbers to divide");
+
+                        number1 = GetNumber();
+                        number2 = GetNumber();
+
+                        Console.WriteLine($"{number1} / {number2} = {calculator.Divide(number1, number2)}");
+                        break;
+
+                    default:
+                        Console.WriteLine("Unknown input");
+                        break;
+                }
             }
+        }
+
+        public static double GetNumber()
+        {
+            var number = Console.ReadLine();
+
+            if (double.TryParse(number, out double doubNum))
+                return doubNum;
+            else
+            {
+                Console.WriteLine("Your input was not a number. Defaulting to 0");
+                return 0.0;
+            } 
         }
     }
 }
